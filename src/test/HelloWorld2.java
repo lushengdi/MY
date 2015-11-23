41aee6fab8f9c501aff13c74ea073a9fa70f170b
lushengdi <285375617@qq.com>
2015-11-23 17:55:53 +0800

    副本

---
+++ src/test/HelloWorld2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloWorld2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
